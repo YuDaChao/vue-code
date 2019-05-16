a1b97fcb5abfb2aa980b194eee75ec319577990a
happyu <290945266@qq.com>
2019-05-16 11:02:44 +0800

    feat: $attrs $listeners

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 
+      text: 一个非 prop 特性是指传向一个组件，但是该组件并没有相应 prop 定义的特性,
+            这些特性会被添加到这个组件的根元素上
+      禁用特性继承: 如果你不希望组件的根元素继承特性，你可以在组件的选项中设置 inheritAttrs: false
+
+     -->
+    <v-props
+      name="happyu"
+      title="study vue"
+      :isActive="true"
+      text="这是一个text"
+      other="this props is not defined"
+      @click="handleClick"
+      @test="handleTest"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VProps from './components/props/Props.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    VProps
+  },
+  methods: {
+    handleClick () {},
+    handleTest () {}
   }
 }
 </script>
---
+++ src/components/props/Props.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="props">
+    <div>接收到父组件的值</div>
+    <ul>
+      <li>使用字符串数组形式列出的 prop</li>
+      <li>title: {{ title }}</li>
+      <li>name: {{ name }}</li>
+      <li>isActive: {{ isActive }}</li>
+      <li>组件没有声明的props: {{ $attrs }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Props',
+  inheritAttrs: false, // 禁用特性继承
+  // props: ['title', 'name', 'isActive'] // 一般不推荐
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    name: {
+      type: String,
+      default: 'happyu',
+      isRequired: true
+    },
+    isActive: {
+      type: Boolean,
+      default: true
+    },
+    click: {
+      type: Function
+    }
+  },
+  mounted () {
+    console.log(this.$props)
+    console.log(this.$listeners)
+  }
+}
+</script>
+
+
